06944f01f28048bfcf5fb66a326bb1dcde12e7f9
patrick <patrick.erath@mail.mcgill.ca>
2019-08-05 18:39:51 +0200

    updates

---
+++ SpeechReco/main.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print('Say Something')
+    audio = r.listen(source)
+
+# niet
+
+try:
+    print(r.recognize_sphinx(audio))
+except Exception as e:
+    print('error '+str(e))
