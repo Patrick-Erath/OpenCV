4eb143b3d6dbde65c507d698d4be53eeb9863871
patrick <patrick.erath@mail.mcgill.ca>
2019-09-17 18:54:43 -0400

    more

--- Tut_2/Tests.py
+++ Tut_2/Tests.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-img_path = "./Pictures/salt.jpeg"
+img_path = "./Pictures/RawPicture.jpg"
 img = cv2.imread(img_path)
 length = 9
 
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
 res1 = cv2.medianBlur(img, length)
-res1 = cv2.cvtColor(res1, cv2.COLOR_BGR2RGB)
 
 res2 = cv2.GaussianBlur(img, (length, length), 0)
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+res1 = cv2.cvtColor(res1, cv2.COLOR_BGR2RGB)
 res2 = cv2.cvtColor(res2, cv2.COLOR_BGR2RGB)
 
 plt.subplot(131), plt.imshow(img)
